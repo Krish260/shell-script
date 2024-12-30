92ad46a087801a7c0c1b24c9f9fe2656ba46e50a
krishna <krishnamurthy741@gmail.com>
2024-12-30 20:38:05 +0530

    conversion2,variables added

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-raji :: hi kittu
-kittu :: hello rajju
+persion1=raji   #no space after and before =
+persion2=kittu
+
+$persion1 :: hi $persion2
+$persion2 :: hello rajju
+$persion1 :: how r u
+$persion2 :: great doing and u
+$persion1 :: well doing $persion2
