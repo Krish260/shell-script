f27664d7a880903a25b1668539dc823463fdd3f6
krishna <krishnamurthy741@gmail.com>
2024-12-31 07:14:07 +0530

    variable change

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,8 +3,8 @@
 persion1=raji   #no space after and before =
 persion2=kittu
 
-$persion1 :: hi $persion2
-$persion2 :: hello rajju
-$persion1 :: how r u
-$persion2 :: great doing and u
-$persion1 :: well doing $persion2
+echo "$persion1 :: hi $persion2"
+echo "$persion2 :: hello rajju"
+echo "$persion1 :: how r u"
+echo "$persion2 :: great doing and u"
+echo "$persion1 :: well doing $persion2"
