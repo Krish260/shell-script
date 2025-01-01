d75a51a6351f6e86b45f019725ccc313b9f27538
krishna <krishnamurthy741@gmail.com>
2025-01-01 07:54:19 +0530

    shell script

--- 04-userinput.sh
+++ 04-userinput.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Please enter your user name::"
-read USERNAME
+read -s USERNAME
 echo "user name entered: $username"
 echo "Please enter your password::"
-read PASSWORD
+read -s PASSWORD
