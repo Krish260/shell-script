cc2397eb7171058ba6f94bd3942e5df45eee39fe
krishna <krishnamurthy741@gmail.com>
2025-01-02 14:06:26 +0530

    script executed

--- 10-inputscriptt.sh
+++ 10-inputscriptt.sh
@@ -7,22 +7,53 @@ then
    echo "ERROR::you must have sudo access to execute the script"
 exit 1 #other than 0
 fi
-
-dnf install mysql -y
-
+dnf list installed mysql
 if [ $? -ne 0 ]
-then 
-  echo "installong mysql...:failure"
-exit 1
-else
-  echo "installing mysql...:success"
-fi
+   then
 
-dnf install git -y
-if [ $? -ne 0 ]
-then 
-  echo "installong git...:failure"
-exit 1
+   dnf install mysql -y
+
+   if [ $? -ne 0 ]
+   then 
+      echo "installong mysql...:failure"
+    exit 1
+    else
+    echo "installing mysql...:success"
+    
 else
-  echo "installing git...:success"
+    echo "mysql already installed..."
+      
 fi
+#dnf install mysql -y
+
+#if [ $? -ne 0 ]
+#then 
+#  echo "installong mysql...:failure"
+#exit 1
+#else
+#  echo "installing mysql...:success"
+#fi
+dnf list installed git 
+if [ $? -ne 0 ]
+then
+     dnf install git -y
+     if [ $? -ne 0 ]
+     then 
+       echo "installong git...:failure"
+     exit 1
+     else
+       echo "installing git...:success"
+     fi
+else
+echo "git already installed...."
+
+
+
+
+#if [ $? -ne 0 ]
+#then 
+#  echo "installong git...:failure"
+#exit 1
+#else
+# echo "installing git...:success"
+#fi
