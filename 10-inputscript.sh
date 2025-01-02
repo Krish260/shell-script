72975e8a9b46fcccd39fa13faecf8dd0b4aac948
krishna <krishnamurthy741@gmail.com>
2025-01-02 13:38:26 +0530

    script executed

--- 10-inputscript.sh
+++ 10-inputscript.sh
@@ -5,7 +5,24 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
    echo "ERROR::you must have sudo access to execute the script"
+exit 1 #other than 0
 fi
 
 dnf install mysql -y
+
+if [ $? -ne 0 ]
+then 
+  echo "installong mysql...:failure"
+exit 1
+else
+  echo "installing mysql...:success"
+fi
+
 dnf install git -y
+if [ $? -ne 0 ]
+then 
+  echo "installong git...:failure"
+exit 1
+else
+  echo "installing git...:success"
+fi
