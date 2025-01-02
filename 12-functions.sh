be407e0a83c24dd255d6640ac6c5b604241e914d
krishna <krishnamurthy741@gmail.com>
2025-01-02 17:43:56 +0530

    script executed

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,15 +1,24 @@
-USERID=(id -u)
-[ $USERID -ne 0 ]
-echo "you should have sudo access"
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+   echo "you should have sudo access to execute the script"
+exit 1
+fi
 dnf list installed mysql
 
+if [ $? -ne 0 ]
+then
    dnf install mysql -y
-   if [ $USERID -ne 0 ]
+   if [ $? -ne 0 ]
    then
       echo "mysql installing...failure"
-      exit1
-   else
-     echo "mysql installing...success"
+      exit 1
+    else
+      echo "mysql installing...success"
    fi
 else
    echo "mysql already installed.... success"
+fi
