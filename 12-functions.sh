601549fd3aa59863a7cc26306a4104f5dc4af6ed
krishna <krishnamurthy741@gmail.com>
2025-01-02 19:36:42 +0530

    script executed

--- 12-functions.sh
+++ 12-functions.sh
@@ -16,27 +16,27 @@ if [ $? -ne 0 ]
 fi  
 }
 
-if [ $USERID -ne 0 ]
-then
-   echo "you should have sudo access to execute the script"
-exit 1
-fi
-dnf list installed mysql
+# if [ $USERID -ne 0 ]
+# then
+#    echo "you should have sudo access to execute the script"
+# exit 1
+# fi
+# dnf list installed mysql
 
-if [ $? -ne 0 ]
-then
-   dnf install mysql -y
-   validate $? "installing mysql"
-else
-   echo -e "mysql already installed.... $y successfully"
-fi
+# if [ $? -ne 0 ]
+# then
+#    dnf install mysql -y
+#    validate $? "installing mysql"
+# else
+#    echo -e "mysql already installed.... $y successfully"
+# fi
 
-dnf list installed git
+# dnf list installed git
 
-if [ $? -ne 0 ]
-then
-   dnf install git -y
-validate $? "git installing"
-else
-   echo -e "git already installed.... $y successfully"
-fi
+# if [ $? -ne 0 ]
+# then
+#    dnf install git -y
+# validate $? "git installing"
+# else
+#    echo -e "git already installed.... $y successfully"
+# fi
